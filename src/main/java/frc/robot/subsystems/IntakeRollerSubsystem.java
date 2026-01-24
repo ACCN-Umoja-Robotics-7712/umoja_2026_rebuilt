7745d6c7b57dad935bce4952f215cc0aca9919fc
Hicf25 <166264982+Hicf25@users.noreply.github.com>
2026-01-24 11:58:25 -0500

    Update IntakeRollerSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeRollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeRollerSubsystem.java
@@ -3,13 +3,10 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import static edu.wpi.first.units.Units.Volts;
 import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeRollerSubsystem extends SubsystemBase {
